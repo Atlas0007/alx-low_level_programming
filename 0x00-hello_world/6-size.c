310997fcc2426b9e1ef6d908f06d9f8279fedbde
Atlas0007 <Eniolaorimalade@gmail.com>
2022-09-08 20:23:57 -0700

    c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,98 +1,14 @@
-betty 6-size.c
-#include <stdio.h>
-
-
-
+#include<stdio.h>
 /**
-
- * main - program starts and ends in this function
-
- * takes no arguments
-
- *
-
- * Description: determine the size of various data types on a computer
-
- * Return: returns 0
-
+ *main-main block
+ *Return: 0
  */
-
 int main(void)
-
 {
-  
-  char c;
-  
-  int i;
-  
-  long int li;
-  
-  long long int lli;
-  
-  float f;
-  
-
-  
-  printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-  
-  printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-  
-  printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
-  
-  printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
-  
-  printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-  
-
-  
-  return (0);
-  
-}#include <stdio.h>
-
-
-
- /**
-
-  * main - program starts and ends in this function
-
-  * takes no arguments
-
-  *
-
-  * Description: determine the size of various data types on a computer
-
-  * Return: returns 0
-
-  */
-
-int main(void)
-
-{
-  
-  char c;
-  
-  int i;
-  
-  long int li;
-  
-  long long int lli;
-  
-  float f;
-  
-
-  
-  printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-  
-  printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-  
-  printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
-  
-  printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
-  
-  printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-  
-
-  
-  return (0);
-  
+  printf("Size of a char: %i byte(s)\n",sizeof(char));
+  printf("Size of an int: %i byte(s)\n",sizeof(int));
+  printf("Size of a long int: %i byte(s)\n",sizeof (long int));
+  printf("Size of a long long int: %i bytes(s)\n",sizeof(long long int));
+  printf("Size of a float: %i bytes(s)\n",sizeof(float));
+  return(0);
 }
