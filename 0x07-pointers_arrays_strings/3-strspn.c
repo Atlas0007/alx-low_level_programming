5d1fee87008cf3f375d7ebff73c1382b166888b4
Atlas0007 <110682552+Atlas0007@users.noreply.github.com>
2022-09-27 00:02:16 +0100

    Create 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring.
+ * @s: Pointer to the string to be examined.
+ * @accept: Pointer to accepted characters of substring.
+ *
+ * Return: Returns number of bytes in initial segment of s,
+ * consisting only of bytes from accept.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, count = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == ' ')
+			break;
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count += 1;
+			}
+		}
+	}
+	return (count);
+}
